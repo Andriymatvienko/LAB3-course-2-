c18aaf821a977698a0ecb9488643275d21c31463
Andriymatvienko <dadaik65@gmail.com>
2023-10-05 23:51:36 +0300

    first commit

---
+++ LAB_3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+class Staff
+{
+    public string Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string NickName { get; private set; }
+    public int Salary { get; private set; }
+
+    public Staff(string id, string firstname, string lastname, string nickname, int salary)
+    {
+        Id = id;
+        FirstName = firstname;
+        LastName = lastname;
+        NickName = nickname;
+        Salary = salary;
+    }
+
+    public void PrintToConsole()
+    {
+        Console.WriteLine($"Робiтник:{Id}");
+        Console.WriteLine($"IМ’Я:{FirstName}");
+        Console.WriteLine($"Фамiлiя:{LastName}");
+        Console.WriteLine($"НiкНейм:{NickName}");
+        Console.WriteLine($"Зарплата:{Salary}");
+    }
+}
+
+class Company
+{
+    public static List<Staff> StaffList = new List<Staff>();
+
+    public static void Add(Staff s)
+    {
+        StaffList.Add(s);
+    }
+
+    public static void Show()
+    {
+        foreach (Staff i in StaffList)
+        {
+            i.PrintToConsole();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"D:\xmlfile.xml");
+
+        foreach (XmlNode node in xml.DocumentElement.ChildNodes)
+        {
+            string id = node.Attributes[0].InnerText;
+            string firstname = node["firstname"].InnerText;
+            string lastname = node["lastname"].InnerText;
+            string nickname = node["nickname"].InnerText;
+            int salary = Int32.Parse(node["salary"].InnerText);
+
+            Company.Add(new Staff(id, firstname, lastname, nickname, salary));
+        }
+
+        Company.Show();
+    }
+}
---
+++ LAB_3_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml;
+
+class Dogs
+{
+    public static List<Dog> DogList = new List<Dog>();
+
+    public static void Add(Dog d)
+    {
+        DogList.Add(d);
+    }
+
+    public static void Show()
+    {
+        foreach (Dog d in DogList)
+        {
+            d.PrintToConsole();
+        }
+    }
+}
+
+class Dog
+{
+    public string DogName { get; private set; }
+    public int DogWeight { get; private set; }
+    public string DogColor { get; private set; }
+
+    public Dog(string dogName, int dogWeight, string dogColor)
+    {
+        DogName = dogName;
+        DogWeight = dogWeight;
+        DogColor = dogColor;
+    }
+
+    public void PrintToConsole()
+    {
+        Console.WriteLine($"Кличка:{DogName}");
+        Console.WriteLine($"Вага:{DogWeight} кг");
+        Console.WriteLine($"Колiр:{DogColor}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"D:\xmlfile1.xml");
+
+        foreach (XmlNode node in xml.DocumentElement.ChildNodes)
+        {
+            string dogName = node["dogName"].InnerText;
+            int dogWeight = Int32.Parse(node["dogWeight"].InnerText);
+            string dogColor = node["dogColor"].InnerText;
+
+            Dogs.Add(new Dog(dogName, dogWeight, dogColor));
+        }
+
+        Dogs.Show();
+    }
+}
